97bc87aef5356141fa94d8c0ef8dec692a33bd19
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-04-01 17:51:33 +1300

    Heroku prep

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 
+// Accessing the path module
+const path = require("path");
+
 // routes
 const books = require('./routes/api/books');
 
@@ -33,6 +36,13 @@ app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
