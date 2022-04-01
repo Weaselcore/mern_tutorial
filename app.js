38472129896428ebc084f496cff0abbcb2a6e340
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-04-01 22:12:09 +1300

    fixed import

--- app.js
+++ app.js
@@ -13,6 +13,7 @@
 
 const express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 // Accessing the path module
